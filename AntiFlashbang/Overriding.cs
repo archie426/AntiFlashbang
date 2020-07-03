d89177401c67158f830b371512748a26a5a2bcde
archie426 <archiestride42@gmail.com>
2020-07-03 12:10:53 +0100

    Clean up

--- AntiFlashbang/Overriding.cs
+++ AntiFlashbang/Overriding.cs
@@ -7,6 +7,43 @@ using Rocket.Core.Logging;
 //Much credits to defcon or kraken or zoomy of whoever
 namespace AntiFlashbang
 {
+    
+    
+    public class OffsetBackup
+    {
+        #region Variables
+        public IntPtr Method;
+
+        public byte A, B, C, D, E, G;
+        public ulong F64;
+        public uint F32;
+        #endregion
+
+        public OffsetBackup(IntPtr method)
+        {
+            Method = method;
+
+            unsafe
+            {
+                byte* ptrMethod = (byte*)method.ToPointer();
+
+                A = *ptrMethod;
+                B = *(ptrMethod + 1);
+                C = *(ptrMethod + 10);
+                D = *(ptrMethod + 11);
+                E = *(ptrMethod + 12);
+                if (IntPtr.Size == sizeof(Int32))
+                {
+                    F32 = *((uint*)(ptrMethod + 1));
+                    G = *(ptrMethod + 5);
+                }
+                else
+                {
+                    F64 = *((ulong*)(ptrMethod + 2));
+                }
+            }
+        }
+    }
     public class OverrideWrapper
     {
         #region Properties
